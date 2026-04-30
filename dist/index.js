2b62a34f733e1f509342269c20d088f6e37ea3ad
fmughalishere <fizamuneer0101@gmail.com>
2026-04-30 14:45:06 +0500

    Updated

--- dist/index.js
+++ dist/index.js
@@ -20,7 +20,8 @@ const io = new Server(server, {
     }
 });
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use('/api/auth', authRoutes);
 app.use('/api/jobs', jobRoutes);
 app.use('/api/applications', applicationRoutes);
