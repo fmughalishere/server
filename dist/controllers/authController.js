e037722e07c20f3613b6c8cde9d3965601c44a27
fmughalishere <fizamuneer0101@gmail.com>
2026-05-12 16:57:07 +0500

    Updated

--- dist/controllers/authController.js
+++ dist/controllers/authController.js
@@ -5,7 +5,7 @@ import nodemailer from 'nodemailer';
 import crypto from 'crypto';
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 587,
+    port: 465,
     secure: false,
     auth: {
         user: process.env.EMAIL_USER,
